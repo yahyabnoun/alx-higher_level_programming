c36ca0341b4b0ac7859f6fe6d5f828602230d7e5
yahya bnoun <yahyaiko22@gmail.com>
2024-05-14 22:11:04 +0100

    push

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states table of hbtn_0e_0_usa where name matches the argument and safe from MySQL injections!"""
+"""  lists all cities from the database hbtn_0e_4_usa """
 import MySQLdb
 import sys
 
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT cities.id,cities.name,states.name FROM cities JOIN states ON states.id = cities.state_id")
+    cur.execute("""SELECT cities.id,cities.name,states.name FROM 
+                cities JOIN states ON states.id = cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
