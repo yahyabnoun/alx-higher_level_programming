f9dedd6932ea90109b7f168dad2cb6e4594f06bf
yahya bnoun <yahyaiko22@gmail.com>
2024-05-14 21:18:19 +0100

    push

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""  lists all states table of hbtn_0e_0_usa where name matches the argument """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name='{}'"
                 .format(sys.argv[4]))
