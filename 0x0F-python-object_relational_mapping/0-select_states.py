497a534caad1331c7c6db18d7b1380f10dfd6df9
Yahya bnoun <yahyabnoun@gmail.com>
2024-05-14 16:15:19 +0100

    add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 import MySQLdb
+import sys
 
 
-
-conn = MySQLdb.connect(host="localhost", port=3306, user="$1", passwd="$2", db="$3", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC") 
 query_rows = cur.fetchall()
