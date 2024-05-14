7e696335c06ef7cae6ecd1f3952ad5f83fcf2c1b
yahya bnoun <yahyaiko22@gmail.com>
2024-05-14 21:57:18 +0100

    push

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  lists all states table of hbtn_0e_0_usa where name matches the argument and safe from MySQL injections!"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
