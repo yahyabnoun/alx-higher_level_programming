bc96b5430bdd0780008efb1c35e0e504c831c227
yahya bnoun <yahyaiko22@gmail.com>
2024-05-15 22:09:04 +0100

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""  lFirst state model """
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
