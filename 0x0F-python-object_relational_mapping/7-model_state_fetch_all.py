6342484d627dc9f0c88fd1b004f0549496c249d8
yahya bnoun <yahyaiko22@gmail.com>
2024-05-17 22:13:26 +0100

    push

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format("$1", "$2", "$3"))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State).all():
+        print(row.id,row.name,sep=": ")
+
+    
