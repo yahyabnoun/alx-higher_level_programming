4c13f4ada975e103c0a012ce2a14f8f8b784bf25
yahya bnoun <yahyaiko22@gmail.com>
2024-05-18 17:00:40 +0100

    push 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ Contains ctate class and Base, an instance of declarative_base()
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
+from sqlalchemy.orm import relationship
 
 class City(Base):
     """
