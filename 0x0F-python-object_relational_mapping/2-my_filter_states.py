cdef3de6eee1f01398508d65eff7f577bbea2b18
yahya bnoun <yahyaiko22@gmail.com>
2024-05-18 13:23:33 +0100

    1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""  lists all states table of hbtn_0e_0_usa where name matches the argument """
+"""  
+lists all states table of hbtn_0e_0_usa 
+where name matches the argument 
+"""
 import MySQLdb
 import sys
 
