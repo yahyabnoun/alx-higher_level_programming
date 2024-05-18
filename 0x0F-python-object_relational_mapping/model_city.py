9c842fc134c360f882411c48d3d8b81bb739bb5d
yahya bnoun <yahyaiko22@gmail.com>
2024-05-18 16:59:32 +0100

    push 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -13,4 +13,4 @@ class City(Base):
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(String(128), ForeignKey('states.id'))
-
+    state = relationship("State", back_populates="citys")
