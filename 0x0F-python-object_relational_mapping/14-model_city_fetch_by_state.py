d358dbe2fd52527a5a367a47f3ab40155926715e
yahya bnoun <yahyaiko22@gmail.com>
2024-05-18 16:55:58 +0100

    push 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    querys = session.query(State.name, City.id, City.name).filter(City.state_id == State.id)
+    for i in querys:
+        print(i)
+    session.commit()
+
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains ctate class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    """
+    Class with id and name attributes of each City
+    """
+    __tablename__ = 'citys'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(128), ForeignKey('states.id'))
+    state = relationship("State", back_populates="citys")
