ecdcb8ff1ec39ac667eb74bbefad5cee1c2d4460
yahya bnoun <yahyaiko22@gmail.com>
2024-05-20 22:17:13 +0100

    all

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
 function add (a, b) {
     return (a + b);
 }
 
-const { argv } = require('process');
 const num1 = parseInt(argv[2]);
 const num2 = parseInt(argv[3]);
 
